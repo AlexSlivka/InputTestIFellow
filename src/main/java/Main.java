7e50ece96bb1934ca5f35d322bb9153ebdf8dfb1
Aleksei Slivka <alexprospero25@gmail.com>
2024-08-10 22:06:51 +0300

    Решение задачи 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("IFellow");
+
+        System.out.println("Задача 1");
+        createRandomArray(11);
+
+        System.out.println("+===========+");
+
+        System.out.println("Задача 2");
+        findFrequentSymbol("Hello");
+
+        System.out.println("+===========+");
+
+    }
+
+    static void createRandomArray(int sizeArray) {
+        int[] ans = new int[sizeArray];
+        for (int i = 0; i < sizeArray; i++) {
+            ans[i] = (int) (Math.random() * sizeArray);
+        }
+        printArray(ans);
+
+        int max = ans[0];
+        int min = ans[0];
+        int sum = 0;
+
+        for (int i = 1; i < sizeArray; i++) {
+            if (ans[i] > max) {
+                max = ans[i];
+            } else if (ans[i] < min) {
+                min = ans[i];
+            }
+            sum += ans[i];
+        }
+
+        System.out.println("MAX = " + max);
+        System.out.println("MIN = " + min);
+        System.out.println("Среднее = " + (sum / sizeArray));
+    }
+
+    static void printArray(int[] ares) {
+        System.out.println("Массив");
+        for (int arr : ares) {
+            System.out.print(arr);
+            System.out.print(" ");
+        }
+        System.out.println(" ");
+    }
+
+    static void findFrequentSymbol(String inputWord) {
+        System.out.println(inputWord);
+        Map<Character, Integer> symbolMap = new HashMap<>();
+        for (char symbol : inputWord.toCharArray()) {
+            if (symbolMap.containsKey(symbol)) {
+                symbolMap.put(symbol, symbolMap.get(symbol) + 1);
+            } else {
+                symbolMap.put(symbol, 1);
+            }
+        }
+
+        char frequentSymbol = ' ';
+        int maxFrequent = 0;
+        for (Map.Entry<Character, Integer> elem : symbolMap.entrySet()) {
+            if (elem.getValue() > maxFrequent) {
+                frequentSymbol = elem.getKey();
+                maxFrequent = elem.getValue();
+            }
+        }
+        System.out.println("Самый повторяющийся символ = " + frequentSymbol);
+
+
+    }
 }
+
